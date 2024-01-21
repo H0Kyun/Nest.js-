a5e9b5894166ddf400cde05bbcdc737d41c9a97a
hokyun <h0kyungim@gmail.com>
2024-01-21 22:16:13 +0900

    board 생성, 조회, 모두 조회, 삭제, 상태 업데이트 추가

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,48 @@
 import { Controller } from '@nestjs/common';
+import { BoardsService } from './boards.service';
+import { Get } from '@nestjs/common';
+import { Board, BoardStatus } from './boards.model';
+import { Post } from '@nestjs/common';
+import { Body } from '@nestjs/common';
+import { CreateBoardDto } from './dto/create-board.dto';
+import { Param } from '@nestjs/common';
+import { Delete } from '@nestjs/common';
+import { Patch } from '@nestjs/common';
+import { UsePipes } from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common';
+import { BoardStatusValidationPipe } from './pipe/board-status-validation.pipe';
 
 @Controller('boards')
-export class BoardsController {}
+export class BoardsController {
+  constructor(private boardsService: BoardsService) {}
+
+  @Get()
+  getAllBoards(): Board[] {
+    return this.boardsService.getAllBoards();
+  }
+
+  @Get('/:id')
+  getBaordById(@Param('id') id: string) {
+    console.log(id);
+    return this.boardsService.getBoardById(id);
+  }
+
+  @Post()
+  @UsePipes(ValidationPipe)
+  createBoard(
+    @Body(new BoardStatusValidationPipe()) createBoardDto: CreateBoardDto,
+  ) {
+    console.log(createBoardDto);
+    return this.boardsService.createBoard(createBoardDto);
+  }
+
+  @Delete('/:id')
+  deleteBoard(@Param('id') id: string) {
+    this.boardsService.deleteBoard(id);
+  }
+
+  @Patch('/:id')
+  updateBoardStatus(@Param('id') id: string) {
+    this.boardsService.updateBoardService(id);
+  }
+}
