fcae2b40c28c8fdc811aa31b6b26a3a7e2f87383
hokyun <h0kyungim@gmail.com>
2024-01-21 22:17:49 +0900

    Board status 값 검증하는 pipe 추가

---
+++ src/boards/pipe/board-status-validation.pipe.ts
@@ -0,0 +1,26 @@
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  Injectable,
+  PipeTransform,
+} from '@nestjs/common';
+import { BoardStatus } from '../boards.model';
+
+@Injectable()
+export class BoardStatusValidationPipe implements PipeTransform {
+  readonly StatusOptions = [BoardStatus.PRIVATE, BoardStatus.PUBLIC];
+
+  transform(value: any, metadata: ArgumentMetadata) {
+    const status = value.status.toUpperCase();
+
+    if (!this.isStatusValid(status)) {
+      throw new BadRequestException(`${status} is not valid status`);
+    }
+
+    return value;
+  }
+
+  private isStatusValid(status: any) {
+    return this.StatusOptions.indexOf(status) !== -1;
+  }
+}
